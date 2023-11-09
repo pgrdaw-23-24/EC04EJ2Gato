6ff47942ad7bd676bd552992774269e730f51d55
pgrdaw <pgrdaw@gmail.com>
2023-11-09 13:49:32 +0100

    modificado gato.js para que contenga sus metodos

--- gato.js
+++ gato.js
@@ -1,35 +1,26 @@
 let gato = {
-    hambre: 0,
-    cansancio: 0,
-    felicidad: 100
+    hambre: 50,
+    cansancio: 50,
+    felicidad: 50,
+    estado: function () {               // devuelve objeto estado
+        return {
+            hambre: this.hambre,
+            cansancio: this.cansancio,
+            felicidad: this.felicidad
+        }
+    },
+    tiempo: function tiempo(t) {        // modifica estado con tiempo
+        this.hambre += aleatorio(t)
+        this.cansancio += aleatorio(t)
+        this.felicidad -= aleatorio(t)
+    },
+    comer: function () { this.hambre -= aleatorio(10) },
+    dormir: function () { this.cansancio -= aleatorio(10) },
+    jugar: function () { this.felicidad += aleatorio(10) },
 }
 
-function aleatorio(num) {
-    return Math.round(Math.random()*num)
+function aleatorio(n) {
+    return Math.round(n / 2 + Math.random() * n)
 }
 
-function comer() {
-    gato.hambre -= aleatorio(10)
-}
-
-function dormir() {
-    gato.cansancio -= aleatorio(10)
-}
-
-function jugar() {
-    gato.felicidad += aleatorio(10)
-}
-
-function tiempo(t) {    
-    gato.hambre += aleatorio(t)
-    gato.cansancio += aleatorio(t)
-    gato.felicidad -= aleatorio(t)
-}
-
-function estado() {
-    console.log(gato)
-}
-
-module.exports = { gato, tiempo, estado, comer, dormir, jugar }
-
-
+module.exports = { gato }
--- testGato.js
+++ testGato.js
@@ -1,14 +1,28 @@
-const gato = require('./gato.js')
+const importarGato = require('./gato.js')
 
-let gato1 = { ...gato }
+let gato1 = { ...importarGato.gato }
+let gato2 = { ...importarGato.gato }
 
-gato1.tiempo(0)
-gato1.estado()
+//inicial
+console.log('\n*** inicial ***')
+console.log('estado inicial gato1:  ',gato1.estado())
+console.log('estado inicial gato2:  ',gato2.estado())
 
-gato1.tiempo(50)
-gato1.estado()
+//gato1
+console.log('\n*** gato1 ***')
+gato1.tiempo(10)
+console.log('pasado un tiempo:      ',gato1.estado())
+gato1.comer(); gato1.dormir(); gato1.jugar()
+console.log('comer, dormir y jugar: ',gato1.estado())
 
-gato1.jugar()
-gato1.comer()
-gato1.dormir()
-gato1.estado()
+//gato2
+console.log('\n*** gato2 ***')
+gato2.tiempo(20)
+console.log('pasado un tiempo:      ',gato2.estado())
+gato2.comer(); gato2.dormir(); gato2.jugar()
+console.log('comer, dormir y jugar: ',gato2.estado())
+
+//final
+console.log('\n*** final ***')
+console.log('estado final gato1:    ',gato1.estado())
+console.log('estado final gato2:    ',gato2.estado())
