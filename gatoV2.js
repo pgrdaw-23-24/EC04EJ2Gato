7d223b3e199cb58cd0b6f99e9ad19a5eb7ffcc84
pgrdaw <pgrdaw@gmail.com>
2023-11-09 19:41:32 +0100

    convertido gato obj en Gato clase y CJS en ES6

---
+++ gatoV2.js
@@ -0,0 +1,26 @@
+function aleatorio(n) {
+    return Math.round(2 * n * Math.random())
+}
+
+let gato = {
+    hambre: 50,
+    cansancio: 50,
+    felicidad: 50,
+    estado: function () {               // devuelve objeto estado
+        return {
+            hambre: this.hambre,
+            cansancio: this.cansancio,
+            felicidad: this.felicidad
+        }
+    },
+    tiempo: function tiempo(t) {        // modifica estado con tiempo
+        this.hambre += aleatorio(t)
+        this.cansancio += aleatorio(t)
+        this.felicidad -= aleatorio(t)
+    },
+    comer: function () { this.hambre -= aleatorio(10) },
+    dormir: function () { this.cansancio -= aleatorio(10) },
+    jugar: function () { this.felicidad += aleatorio(10) },
+}
+
+module.exports = { gato }
--- testGato.js
+++ testGato.js
@@ -1,20 +1,27 @@
-const importarGato = require('./gato.js')
+import Gato from './gato.js'
 
-let gato1 = { ...importarGato.gato }
-let gato2 = { ...importarGato.gato }
+let gato1 = new Gato()
+let gato2 = new Gato()
 
-console.log('\n*** inicial ***')
-console.log('estado gato1:',gato1.estado())
-console.log('estado gato2:',gato2.estado())
+console.log('*** estado inicial ***')
+console.log('estado gato1:', gato1.estado())
+console.log('estado gato2:', gato2.estado())
+
+console.log('\n*** modificar estado ***')
+gato1.hambre = 30; gato1.cansancio = 40; gato1.felicidad = 70;
+gato1.hambre = 10; gato1.cansancio = 20; gato1.felicidad = 80;
+
+console.log('estado gato1:', gato1.estado())
+console.log('estado gato2:', gato2.estado())
 
 console.log('\n*** pasado un tiempo ***')
 gato1.tiempo(10)
 gato2.tiempo(10)
-console.log('estado gato1:',gato1.estado())
-console.log('estado gato2:',gato2.estado())
+console.log('estado gato1:', gato1.estado())
+console.log('estado gato2:', gato2.estado())
 
 console.log('\n*** comer, dormir y jugar ***')
 gato1.comer(); gato1.dormir(); gato1.jugar()
 gato2.comer(); gato2.dormir(); gato2.jugar()
-console.log('estado gato1:',gato1.estado())
-console.log('estado gato2:',gato2.estado())
+console.log('estado gato1:', gato1.estado())
+console.log('estado gato2:', gato2.estado())
